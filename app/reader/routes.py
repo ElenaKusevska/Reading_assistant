8fb7f5685b48dd65e0dd7a515861e481f2e2cb9d
ElenaKusevska <elena.kusevska@gmail.com>
2024-10-08 03:34:30 +0200

    move save uploaded file to utils.py

--- app/reader/routes.py
+++ app/reader/routes.py
@@ -10,7 +10,7 @@ from gtts import gTTS
 
 from .pdf_functions import parse_pdf_file
 from .txt_functions import parse_txt_file
-from .utils import clear_directory
+from .utils import save_uploaded_file
 
 
 # First view of web app (when it's loaded for the first time):
@@ -25,32 +25,20 @@ def upload():
 
     # Get file info from HTML form:
     uploaded_file = request.files['file']
-    uploaded_file_name = uploaded_file.filename
 
     # Save and read file:
-    if (uploaded_file_name != ''):
+    if (uploaded_file.filename != ''):
 
-        # Clean up file upload directory if it exists,
-        # or create it if it doesn't exist:
-        file_upload_dir_path = os.path.join(os.getcwd(),"uploaded_file")
-        if not os.path.isdir(file_upload_dir_path):
-            os.mkdir(file_upload_dir_path)
-        else:
-            clear_directory(file_upload_dir_path)
+        uploaded_file_path = save_uploaded_file(uploaded_file)
 
-        # Save the file:
-        uploaded_file_path = os.path.join(file_upload_dir_path,
-            uploaded_file_name)
-        uploaded_file.save(uploaded_file_path)
-
-        if ".txt" in uploaded_file_name:
+        if ".txt" in uploaded_file.filename:
             file_text, audio_text = parse_txt_file(uploaded_file_path)
 
             number_of_audio_files = 1
             audiofilename = '1.mp3'
             myobj = gTTS(text=audio_text, lang='en', slow=False)
             myobj.save(os.getcwd() + url_for('static', filename=audiofilename))
-        elif ".pdf" in uploaded_file_name:
+        elif ".pdf" in uploaded_file.filename:
             file_text, audio_lines, naudio = parse_pdf_file(uploaded_file_path)
 
         return jsonify({"success": True, "audio_file": "1.mp3", "number_of_audio_files": naudio, "file_text": file_text})
--- app/reader/utils.py
+++ app/reader/utils.py
@@ -1,6 +1,24 @@
 import os
 
-def clear_directory(dir_path):
+
+def save_uploaded_file(uploaded_file):
+    file_upload_dir = os.path.join(os.getcwd(),"uploaded_file")
+    clean_or_create_directory(file_upload_dir)
+
+    # Save the file:
+    uploaded_file_path = os.path.join(file_upload_dir,
+        uploaded_file.filename)
+    uploaded_file.save(uploaded_file_path)
+
+    return uploaded_file_path
+
+def clean_or_create_directory(dir_path):
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+    else:
+        clean_directory(dir_path)
+
+def clean_directory(dir_path):
     stuff_in_dir = os.listdir(dir_path)
     for athing in stuff_in_dir:
         athings_path = os.path.join(dir_path,athing)
